b4b99d13a95691fcedbc587e85b1a1361c4a305a
Preston Jenkins <prestonjenkins@Prestons-MacBook-Pro.local>
2020-01-21 13:55:13 -0700

    Yatta

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-function addTodo(event){
-    event.preventDefault();
-    const item = document.createElement('li');
-    item.innerText = document.getElementById('item').value;
-    item.addEventListener('click', completeTodo);
+console.log('connected')
 
-    const button = document.createElement('button');
-    button.innerText = 'x';
+
+function addTodo(event) {
+    event.preventDefault()
+    const item = document.createElement('li')
+    item.innerText = document.getElementById('item').value
+    item.addEventListener('click', completeTodo)
+
+    const button = document.createElement('button')
+    button.innerText = 'x'
     button.addEventListener('click', removeTodo)
-    item.appendChild(button);
-
-    const list = document.querySelector('ul');
+    item.append(button)
+    
+    const list = document.querySelector('ul')
     list.appendChild(item)
 }
 
-document.querySelector('form').addEventListener('submit', addTodo);
-
-function removeTodo(event){
+function removeTodo (event) {
     event.target.parentNode.remove()
 }
 
-function completeTodo(event){
-    const checked = event.target.getAttribute('aria-checked')
-    if(checked === 'true'){
-        event.target.setAttribute('aria-checked', 'false')
-    } else {
+function completeTodo (event) {
+    const value = event.target.getAttribute('aria-checked')
+
+    if(value !== "true") {
         event.target.setAttribute('aria-checked', 'true')
+    } else {
+        event.target.setAttribute('aria-checked', 'false')
     }
 }
+
+document.querySelector("form").addEventListener("submit", addTodo)
+
